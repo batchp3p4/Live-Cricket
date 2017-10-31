da915e2bef2e51c76a767bbe7067e6f25d4c2921
Shubham Atriwal <atriwal.shubham25@gmail.com>
2017-10-31 23:32:40 +0530

    Update live_cricket.py

--- live_cricket.py
+++ live_cricket.py
@@ -62,5 +62,5 @@ def main():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='1.1.1.1', port=port, debug=True)
 
